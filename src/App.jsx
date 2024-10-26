71a37c971eb2f89c77b06b843d3143da76189a08
Ayush <tarunsingh78490@gmail.com>
2024-10-27 03:07:58 +0530

    connection to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+'use client'
+
+import { useTonConnectUI} from '@tonconnect/ui-react'
+import { TonConnectUI } from '@tonconnect/ui-react'
+import { Children, useCallback, useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tonConnectUI] = useTonConnectUI();
+  const [tonWalletAddress, setTonWalletAddress] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  const handleWalletConnection = useCallback((address) => {
+    setTonWalletAddress(address);
+    console.log("wallet connected successfully");
+    setIsLoading(false);
+  }, []);
+
+  const handleWalletDisConnection = useCallback((address) => {
+    setTonWalletAddress(null);
+    console.log("wallet Disconnected successfully");
+    setIsLoading(false);
+  }, []);
+  useEffect (()=>{
+    const checkWalletConnection = async () =>{
+      if(tonConnectUI.account?.address){
+        handleWalletConnection(tonConnectUI.account?.address)
+      }else{
+        handleWalletDisConnection();
+      }
+    };
+    checkWalletConnection();
+
+    const unSubscribe = tonConnectUI.onStatusChange((wallet) => {
+      if(wallet){
+        handleWalletConnection(wallet.account.address)
+      }else{
+        handleWalletDisConnection();
+      }
+    });
+
+    return () => {
+      unSubscribe();
+    }
+
+  }, [tonConnectUI, handleWalletConnection, handleWalletDisConnection]);
+
+  const handleWalletAction = async () => {
+    if(tonConnectUI.connected){
+      setIsLoading(true);
+      await tonConnectUI.disconnect();
+    }else{
+      await tonConnectUI.openModal();
+    }
+  };
+
+  const formatAddress = (address) => {
+    const tempAddress = Address.parse(address).toString();
+    return '${tempAddress.slice(0,4)...${tempAddress.slice(-4)}}'
+  };
+
+  if(isLoading){
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="bg-grey-200 text-grey-700 font-bold py-2  px-4 rounded">
+          Loading...
+        </div>
+
+      </main>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className='flex main-h-screen flex-coloumn item-center justify-center'>
+      <h1 className='text-4xl font-bold mb-8'>Ton Connect Demo</h1>
+      {tonWalletAddress? (
+        <div className='flex flex-col item-center'>
+          <p className='mb-4'>Connected:{formatAddress(tonWalletAddress)}</p>
+          <button
+          onClick={handleWalletAction}
+          className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Disconnect Wallet
+          </button>
+
+        </div>
+      ):(
+        <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+          Connect TON wallet
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+
+    </main>
+  </> 
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  <TonConnectUIProvider manifestUrl="https://gray-personal-armadillo-107.mypinata.cloud/ipfs/QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH">
+        <App/>
+      </TonConnectUIProvider>
+ 
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
 )
